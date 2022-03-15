4f6d88cb9ab5a2acb5263085449a874bc18c1483
Steffen Coenen <steffen.coenen@web.de>
2022-03-14 23:53:43 -0700

    Added video on home page

--- src/home.py
+++ src/home.py
@@ -6,9 +6,14 @@ def write(data):
 	
 	st.title("Data Visualization Tool using the GeoLife Mobility and Weather Data for Beijing, China 🌆")
 	
-	st.write("A Python-based application utilizing Streamlit to provide structured, interactive visualizations on the [GeoLife GPS Trajectories dataset](https://www.microsoft.com/en-us/download/details.aspx?id=52367&from=https%3A%2F%2Fresearch.microsoft.com%2Fen-us%2Fdownloads%2Fb16d359d-d164-469e-9fd4-daa38f2b2e13%2F).")
+	st.write("This website is a Python-based application utilizing Streamlit to provide structured, interactive visualizations on the [GeoLife GPS Trajectories dataset](https://www.microsoft.com/en-us/download/details.aspx?id=52367&from=https%3A%2F%2Fresearch.microsoft.com%2Fen-us%2Fdownloads%2Fb16d359d-d164-469e-9fd4-daa38f2b2e13%2F).")
 	
-	st.write("Geolife Trajectory data set contains information of 3 entities \n + GPS Observation \n + Ground Truth \n + Trips \n Along with these entities, the Weather entity is connected to understand the mode choice based on the different weather conditions and other factors.")
+	st.write("Geolife Trajectory data set was recorded between April 2007 and December 2011. It contains information of 3 entities \n + GPS Observation \n + Ground Truth \n + Trips \n Along with these entities, we connected a Weather entity set to understand the mode choice based on the different weather conditions and other factors.")
 	
-	#TODO: include some basic model outcomes (incl. odds ratios)
+	st.write("Have fun exploring the data! 💡")
 	
+	video_file = open('img/demo.webm', 'rb')
+	video_bytes = video_file.read()
+	
+	st.write("**Demonstration video of this app:**")
+	st.video(video_bytes)	
